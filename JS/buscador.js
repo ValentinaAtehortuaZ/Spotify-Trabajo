ea44dd689d5e28e0b2312d2403fc7676400cec71
ValentinaAtehortuaZ <valentinaatehortuaz@gmail.com>
2022-03-30 14:22:57 -0500

    Se ha modificado el formulario (da)

--- JS/buscador.js
+++ JS/buscador.js
@@ -1,9 +1,37 @@
-function capturetext(){
-    let capturetext = document.getElementById("buscador")
-  
-    console.log(capturetext)
+import {generarURI} from "./generarArtistas.js"
+
+export function buscarArtista(buscador){
+    let capturetext = document.getElementById("cajaBusqueda").value;
+    let ID
+    
+   if(capturetext.toLowerCase()==("Miley Cyrus".toLowerCase())){
+    ID="5YGY8feqx7naU7z4HrwZM6"
+    
+     
+   }
+
+   else if(capturetext.toLowerCase()==("Adele".toLowerCase())){
+    ID="4dpARuHxo51G3z768sgnrY"
+    
+   }
+   else if(capturetext.toLowerCase()==("hayley kiyoko")){
+     ID="3LjhVl7GzYsza1biQjTpaN"
+   }
+   else if(capturetext.toLowerCase()==("kany garcia")){
+    ID="69UypehHabb68utzfjAVlV"
+  }
+  else if(capturetext.toLowerCase()==("nathy peluso")){
+    ID="3VHAySZQPlfGlNLslzXYpN"
   }
 
+  
+return(ID)
+
+  
+   
+  }
+
+  generarURI()
 
 
 
--- JS/controlador.js
+++ JS/controlador.js
@@ -2,19 +2,21 @@ import {pintarCanciones} from "./pintarCanciones.js"
 import {generarURI} from "./generarArtistas.js"
 import {generarTOKEN} from "./generadorToken.js"
 import {consumirAPI} from "./servicios.js"
+import { buscarArtista} from "./buscador.js"
 
 let cards=document.getElementById("contenedorCanciones")
 
 let boton= document.getElementById("boton")
-boton.addEventListener("click",function(evento){
+boton.addEventListener("click",function(event){
 
+    event.preventDefault()
 
-    evento.preventDefault()
-
-    let artistaSeleccionada= document.getElementById("artista").value
-
-    let URI=generarURI(artistaSeleccionada)
-
+    //let artistaSeleccionada= document.getElementById("artista").value
+   
+   let artistaSelecciondo=buscarArtista()
+   // let URI=generarURI(artistaSeleccionada)
+    let URI=generarURI(artistaSelecciondo)
+    console.log(artistaSelecciondo)
 
 
     
@@ -27,7 +29,7 @@ boton.addEventListener("click",function(evento){
       
         pintarCanciones(datosConsultadosAPI)
     }
-
+   artistaSelecciondo=""
     activarAPI()
     cards.innerHTML=""
 
